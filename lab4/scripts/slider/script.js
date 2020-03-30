eaa9c3e31adc1ed70e48dc86b830063d5b12ece9
Viacheslav <little.cado1@gmail.com>
2020-03-30 20:35:30 +0300

    Add slider responsive

--- lab4/scripts/slider/script.js
+++ lab4/scripts/slider/script.js
@@ -3,26 +3,55 @@ $(document).ready(function () {
         speed: 300,
         slidesToShow: 4,
         infinite: true,
+        centerMode: true,
         responsive: [
           {
             breakpoint: 1440,
             settings: {
               slidesToShow: 3,
               slidesToScroll: 1,
+              centerMode: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              centerMode: false,
               dots: true
             }
           },
           {
             breakpoint: 1000,
             settings: {
-              slidesToShow:                                          2,
+              slidesToShow:2,
+              centerMode: true,
               dots: true
             }
           },
           {
-            breakpoint: 680,
+            breakpoint: 800,
+            settings: {
+              slidesToShow:2,
+              centerMode: false,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
             settings: {
               slidesToShow: 1,
+              centerMode: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 450,
+            settings: {
+              slidesToShow: 1,
+              centerMode: false,
               dots: true
             }
           }
